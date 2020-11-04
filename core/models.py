732fba2a325eb7e9910b8b48683c53ff01cab937
Kim Leibowitz <kim@brightlightwellness.com>
2020-11-04 14:21:40 -0500

    update models

--- core/models.py
+++ core/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
-class Habit_detail(models.Model):
+class Habit(models.Model):
     name = models.CharField(max_length=255)
     goal_target = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='habits')
 
-class daily_Record(models.Model):
+class daily_record(models.Model):
     entry = models.TextField()
     date = models.DateField()
-    habit = models.ForeignKey(Habit_detail, on_delete=models.CASCADE, related_name="dailyRecords")
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE, related_name="dailyRecords")
